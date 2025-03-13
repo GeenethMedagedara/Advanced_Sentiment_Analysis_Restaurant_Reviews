61b38335da0fa484330e08e95b5f480a392022c3
GeenethM <geenethmedagedara@gmail.com>
2025-03-13 15:47:15 +0530

    connect to mongo

---
+++ config/db_config.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+# MongoDB Configuration
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")  # Change if hosted elsewhere
+DATABASE_NAME = "predictions_db"
+
+def get_db():
+    """Establish a connection to MongoDB and return the database instance."""
+    client = MongoClient(MONGO_URI)
+    db = client[DATABASE_NAME]
+    return db
